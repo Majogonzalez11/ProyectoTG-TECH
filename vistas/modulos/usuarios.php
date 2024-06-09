150d6d9aa43990bb71690b5a0eda3502d7946ce9
Majogonzalez11 <gonzalezmariajose2807gmail.com>
2024-06-09 10:55:32 -0300

    probando cambio 99

--- vistas/modulos/usuarios.php
+++ vistas/modulos/usuarios.php
@@ -1,6 +1,6 @@
-<div class="content-wrapper" style="background-color: #9E63B8;">
+<div class="content-wrapper" style="background-color: #EAEAEA;">
 
-    <section class="content header" style="background-color: #9E63B8;">
+    <section class="content header" style="background-color: #5A3E8A;">
         <h1 style="color: white;">
             Administrar usuarios
         </h1>
@@ -13,9 +13,9 @@
 
 <section class="content">
     
-    <div class="box" style="background-color:#DABAEA;">
-        <div class="box-header with-border" style="background-color:#DABAEA;">
-            <button class="btn btn primary" data-toggle="modal" data-target="#modalAgregarUsuario" style= "background-color: lightblue;">
+    <div class="box" style="background-color:#EAEAEA;">
+        <div class="box-header with-border" style="background-color:#EAEAEA;">
+            <button class="btn btn primary" data-toggle="modal" data-target="#modalAgregarUsuario" style= "background-color: #EAEAEA;">
                 Agregar Usuario
             </button>
         </div>
@@ -36,7 +36,7 @@
              </thead>
              <!--Maquetacion de la tabla de usuarios realizada con bootstrap --> 
              <tbody > 
-                <tr style="background-color: #DABAEA;">
+                <tr style="background-color: #EAEAEA;">
                     <td>1</td>
                     <td>Usuario Administrador</td>
                     <td>admin</td>
@@ -44,14 +44,14 @@
                     <td>administrador</td>
                     <td>
                         <div class="btn-group">
-                            <button class="btn btn-warning" style="background-color: #6E4684;"><i class="fa fa-pencil"></i> </button>
+                            <button class="btn btn-warning"><i class="fa fa-pencil"></i> </button>
                             <button class="btn btn-danger"><i class="fa fa-times"></i> </button>
                         </div>
                     </td>
                 </tr>
              </tbody>
              <tbody>
-                <tr style="background-color: #DABAEA;">
+                <tr style="background-color: #EAEAEA;">
                     <td>2</td>
                     <td>Usuario Vendedor</td>
                     <td>admin2</td>
@@ -59,7 +59,7 @@
                     <td>vendedor</td>
                     <td>
                         <div class="btn-group">
-                            <button class="btn btn-warning" style="background-color: #6E4684;"><i class="fa fa-pencil" ></i> </button>
+                            <button class="btn btn-warning"><i class="fa fa-pencil" ></i> </button>
                             <button class="btn btn-danger"><i class="fa fa-times"></i> </button>
                         </div>
                     </td>
@@ -74,11 +74,11 @@
 <!--(bootstrap) modal agregar usuario-->
 
 <div id="modalAgregarUsuario" class="modal fade" role="dialog">
-    <div class="modal-dialog" style="background-color: #C5A5D4;" >
-        <div class="modal-content" style="background-color: #C5A5D4;">
+    <div class="modal-dialog" style="background-color: #EAEAEA;" >
+        <div class="modal-content" style="background-color: #EAEAEA;">
             
         <div class="modal-header" >
-                <button type="button" class="close" data-dismiss="modal" style="color: #C5A5D4;">&times;</button>
+                <button type="button" class="close" data-dismiss="modal" style="color:#EAEAEA;">&times;</button>
                 <h4 class="modal-title">Agregar usuario</h4>
             </div>
             
